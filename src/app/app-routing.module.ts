59e0c17332493c92bcc7f42f3b49e12712aef3fb
Viraj Malap <viraj.m@thinkbridge.in>
2024-12-02 18:33:14 +0530

    Added guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,20 +10,21 @@ import { ExamResultComponent } from './User/exam-result/exam-result.component';
 import { ExamHistoryComponent } from './User/exam-history/exam-history.component';
 import { StudentDashboardComponent } from './User/student-dashboard/student-dashboard.component';
 import { AdminDashboardComponent } from './Admin/admin-dashboard/admin-dashboard.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  { path: 'user-dashboard', component: WelcomeComponent }, 
-  { path: 'teacher-dashboard', component: WelcomeComponent }, 
+  { path: 'user-dashboard', component: WelcomeComponent,canActivate: [AuthGuard] }, 
+  { path: 'teacher-dashboard', component: WelcomeComponent,canActivate: [AuthGuard]  }, 
   { path: 'login', component: LoginComponent }, 
   { path: 'register', component: WelcomeComponent }, 
-  { path: 'admin-exam', component: AdminExamComponent }, 
-  { path: 'add-exam', component: AddExamComponent }, 
-  { path: 'exams', component: UpcomingExamsComponent}, 
-  { path: 'exam-questions', component: ExamQuestionComponent }, 
-  { path: 'exam-results', component: ExamResultComponent }, 
-  { path: 'exam-history', component: ExamHistoryComponent }, 
-  { path: 'student-dashboard', component: StudentDashboardComponent },
-  { path: 'admin-dashboard', component: AdminDashboardComponent },
+  { path: 'admin-exam', component: AdminExamComponent,canActivate: [AuthGuard]  }, 
+  { path: 'add-exam', component: AddExamComponent,canActivate: [AuthGuard]  }, 
+  { path: 'exams', component: UpcomingExamsComponent,canActivate: [AuthGuard] }, 
+  { path: 'exam-questions', component: ExamQuestionComponent,canActivate: [AuthGuard]  }, 
+  { path: 'exam-results', component: ExamResultComponent,canActivate: [AuthGuard]  }, 
+  { path: 'exam-history', component: ExamHistoryComponent,canActivate: [AuthGuard]  }, 
+  { path: 'student-dashboard', component: StudentDashboardComponent,canActivate: [AuthGuard]  },
+  { path: 'admin-dashboard', component: AdminDashboardComponent,canActivate: [AuthGuard]  },
   { path: '**', redirectTo: 'login', pathMatch: 'full' }, 
 ];
 
